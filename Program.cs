023da004aa8cef39184b6280e025247d66c0e445
Raul2114 <149166822+Raul2114@users.noreply.github.com>
2024-04-05 21:50:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassVsStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Класс
+            ClassClass classObject = new ClassClass();
+            classObject.a = 1;
+            ClassClass classObject2 = classObject;
+            classObject2.a = 2;
+            Console.WriteLine($"classObject: {classObject.a}, classObject2: {classObject2.a}");
+
+            // Структура
+            StructStruct structObject = new StructStruct();
+            structObject.SomeValue = 1;
+            StructStruct structObject2 = structObject;
+            structObject2.SomeValue = 2;
+            Console.WriteLine($"structObject: {structObject.SomeValue}, structObject2: {structObject2.SomeValue}");
+
+            Console.ReadKey();
+        }
+    }
+
+    // Класс
+    class ClassClass
+    {
+        public int a;
+    }
+
+    // Структура
+    struct StructStruct
+    {
+        public int SomeValue;
+    }
+}
